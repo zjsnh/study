bc03fc10b59e03345040b9b58297c67da7e9e61c
zjsnh <144636055+zjsnh@users.noreply.github.com>
2024-04-07 17:49:41 +0800

    text

---
+++ text/Project2/Project2/text.cpp
@@ -0,0 +1,62 @@
+//for (int i = 0; i < _size - 1; i++)
+//{
+//	if (_data[i] < _data[i + 1])
+//	{
+//		swap(_data[i], _data[i + 1]);
+//
+//	}
+//}
+
+//for (int i = 0; i < _size - 1; i++) {
+//    for (int j = 0; j < size - i - 1; j++) {
+//        if (_data[j] > _data[j + 1]) {
+//            swap(_data[j], _data[j + 1]);
+//        }
+//    }
+//}
+
+#include<iostream>
+using namespace std;
+
+//namespace A
+//{
+//	int a = 0;
+//}
+//namespace B
+//{
+//	int a = 1;
+//}
+
+//void swap(int* p1, int* p2)
+//{
+//	int temp = *p1;
+//	*p1 = *p2;
+//	*p2 = temp;
+//}
+//
+//void swap(int& p1, int& p2)
+//{
+//	int temp = p1;
+//	p1 = p2;
+//	p2 = temp;
+//}
+
+//void add(int a, int b)
+//{
+//	cout<< a + b;
+//}
+//
+//void add(double a)
+//{
+//	cout<< a;
+//}
+//
+//
+//int main()
+//{
+//	
+//	add(1, 2);
+//	add(1.2);
+//
+//	return 0;
+//}
