cbc2e4257cf11f91855ef9425e1a5c3eefc116c7
zjsnh <144636055+zjsnh@users.noreply.github.com>
2024-07-17 21:46:54 +0800

    oj

--- c++_study/ostext/ostext/text.cpp
+++ c++_study/ostext/ostext/text.cpp
@@ -98,42 +98,229 @@ using namespace std;
 //}
 
 
-bool Jump(vector<int>& nums, int i)
-{
-    if (i >= nums.size()-1)
-    {
-        return true;
-    }
+//bool Jump(vector<int>& nums, int i)
+//{
+//    if (i >= nums.size()-1)
+//    {
+//        return true;
+//    }
+//
+//    int nextPos = -1;
+//    for (int j = i + 1; j <= min(i + nums[i], (int)nums.size() - 1); j++)
+//    {
+//        if (nums[j] > nums[i])
+//        {
+//            nextPos = j;
+//            break;
+//        }
+//    }
+//
+//    if (nextPos == -1) // 如果没有找到比当前数大的数，则无法前进
+//    {
+//        return false;
+//    }
+//
+//    return Jump(nums, nextPos); // 递归跳到下一个位置
+//
+//}
+//
+//bool canJump(vector<int>& nums) {
+//    
+//    int i = 0;
+//    return Jump(nums, i);
+//}
+//
+//
+//int main()
+//{
+//    vector<int> nums = { 2,3,1,1,4 };
+//    cout<< canJump(nums);
+//    return 0;
+//}
 
-    int nextPos = -1;
-    for (int j = i + 1; j <= min(i + nums[i], (int)nums.size() - 1); j++)
+
+#include <iostream>
+#include<vector>
+using namespace std;
+//int main() {
+//    vector<int> V = { 1,100, 1, 1, 1, 90, 1, 1, 80, 1 };
+//    int a = 10;
+//
+//    if (V.empty()) {
+//        cout << 0;
+//        return 0;
+//    }
+//
+//    int budget = V[0];
+//    int c = 0;
+//
+//    if (V.size() > 1) {
+//        if (V[0] < V[1]) {
+//            budget = V[0];
+//            c = 0;
+//        }
+//        else {
+//            budget = V[1];
+//            c = 1;
+//        }
+//    }
+//
+//    for (int i = c; i < V.size();) {
+//        if (i + 1 >= V.size()) {
+//            cout << budget;
+//            return 0;
+//        }
+//
+//        if (i + 2 >= V.size()) {
+//            cout << budget;
+//            return 0;
+//        }
+//
+//        if (V[i + 1] < V[i + 2]) {
+//            budget += V[i + 1];
+//            i += 1;
+//        }
+//        else {
+//            budget += V[i + 2];
+//            i += 2;
+//        }
+//    }
+//
+//    cout << budget;  // 确保输出最终预算值
+//    return 0;
+//}
+
+//#include<queue>
+//
+//int main() {
+//
+//   /* int c;
+//    cin >> c;*/
+//    int a = 3, b;
+// /*   cin >> a;*/
+//
+//    priority_queue<int> Q;
+//    vector<int> V = { 2, 4, 8, 10, 11 };
+//
+//
+//    //while (cin >> b) {
+//    //    V.push_back(b);
+//    //}
+//
+//    int num = 0;
+//
+//    for (auto e : V)
+//    {
+//        if ((e & 1) == 0)
+//        {
+//            Q.push(e);
+//        }
+//        else
+//        {
+//            num += e;
+//        }
+//    }
+//
+//    for (int i = 0; i < a && !Q.empty(); i++)
+//    {
+//        while ((Q.top() & 1) != 0 && !Q.empty())
+//        {
+//            num += Q.top();
+//            Q.pop();
+//        }
+//
+//        if (Q.empty())
+//        {
+//            break;
+//        }
+//      
+//        int temp = Q.top() / 2;
+//        Q.pop();
+//        Q.push(temp);
+//      
+//    }
+//
+//    while (!Q.empty())
+//    {
+//        num += Q.top();
+//        Q.pop();
+//    }
+//
+//    cout << num << endl;
+//
+//
+//    return 0;
+//}
+
+int main() {
+    //int L;
+   // cin >> L;
+
+    int critical = 20;
+   // cin >> critical;
+
+    vector<int> V = { 1, 1, 6, 10, 9, 3, 3, 5, 3, 7 };
+
+    /*int b;
+    while (cin >> b)
     {
-        if (nums[j] > nums[i])
+        V.push_back(b);
+    }*/
+
+    int mindistance = V.size();
+    int right = 0;
+    int left = 0;
+
+    pair<int, int> LR_Location;
+
+    int num = 0;
+
+    /*for (int i = 0; i < V.size(); )
+    {
+        if (num >= critical)
         {
-            nextPos = j;
-            break;
+            if (right - left + 1 < mindistance)
+            {
+                LR_Location = pair<int, int>(left, right);
+                mindistance = right - left + 1;
+            }
+
+            while (left <= right && num > critical)
+            {
+                num -= V[left];
+                if (num > critical)
+                {
+                    left++;
+
+                    LR_Location = pair<int, int>(left, right);
+                    --mindistance;
+                }
+            }
         }
+        else
+        {
+            num += V[i];
+            right = i;
+            i++;
+        }
+    }*/
+
+    while (right < V.size()) {
+        num += V[right];
+
+        while (num >= critical) {
+            if (right - left + 1 < mindistance) {
+                LR_Location = pair<int, int>(left, right);
+                mindistance = right - left + 1;
+            }
+            num -= V[left];
+            left++;
+        }
+        right++;
     }
 
-    if (nextPos == -1) // 如果没有找到比当前数大的数，则无法前进
-    {
-        return false;
-    }
-
-    return Jump(nums, nextPos); // 递归跳到下一个位置
-
-}
-
-bool canJump(vector<int>& nums) {
-    
-    int i = 0;
-    return Jump(nums, i);
-}
-
-
-int main()
-{
-    vector<int> nums = { 2,3,1,1,4 };
-    cout<< canJump(nums);
+    cout << LR_Location.first + 1 << " " << LR_Location.second + 1 << endl;
     return 0;
+
+
 }
