a387d65f0027ea87f3c86830c214bcf5825b214f
zjsnh <144636055+zjsnh@users.noreply.github.com>
2024-09-11 21:26:58 +0800

    const_cast

--- c++_study/三数之和/三数之和/text.cpp
+++ c++_study/三数之和/三数之和/text.cpp
@@ -97,24 +97,77 @@ using namespace std;
 //    cout << lengthOfLongestSubstring(s) << endl;
 //    return 0;
 //}
+//#include<unordered_map>
+//vector<int> findAnagrams(string s, string p) {
+//
+//    vector<int> answer;
+//    int w_start = 0, w_end = p.size() - 1;
+//    unordered_map<char, int> M1;
+//    unordered_map<char, int> M2;
+//
+//    for (int i = w_start; i <= w_end; ++i) {
+//        M1[s[i]]++;
+//    }
+//    for (int i = 0; i < p.size(); ++i) {
+//        M2[p[i]]++;
+//    }
+//
+//    while (w_end < s.size()) {
+//        if (M1 == M2) {
+//            answer.emplace_back(w_start);
+//        }
+//
+//        M1[s[w_start++]]--;
+//        if (M1[s[w_start - 1]] == 0) 
+//            M1.erase(s[w_start - 1]);
+//
+//        M1[s[++w_end]]++;
+//    }
+//
+//    return answer;
+//
+//}
+//
+//int main()
+//{
+//    string s("cbaebabacd");
+//    string p("abc");
+//
+//    findAnagrams(s, p);
+//
+//    return 0;
+//}
 
-#include<unordered_set>
 
-vector<int> findAnagrams(string s, string p) {
+//
+//int subarraySum(vector<int>& nums, int k) {
+//
+//
+//	for (int i = 0; i < nums.size(); i++)
+//	{
+//
+//	}
+//	
+//}
+//
+//int main()
+//{
+//	vector<int> nums = { 1,2,3 };
+//	subarraySum(nums, 3);
+//	return 0;
+//}
 
-	for (int i = 0; i < s.size() - p.size(); i++)
-	{
-		for (int j = i; j < p.size(); j++)
-		{
-			if()
-		}
-	}
+#include <iostream>
 
-}
+int main() {
+    const int i = 0;
+    int& r = const_cast<int&>(i);
+    r = 1;
+    std::cout << &r << endl;
+    std::cout << &i << endl;
 
-int main()
-{
+    std::cout << r << endl;
+    std::cout << i;
 
-
-	return 0;
+    return 0;
 }
