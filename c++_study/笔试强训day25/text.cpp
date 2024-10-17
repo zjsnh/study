eef009e1be22a41fec9884c745c20ebecc5ec29e
zjsnh <144636055+zjsnh@users.noreply.github.com>
2024-10-17 21:47:47 +0800

    socket

---
+++ c++_study/笔试强训day25/text.cpp
@@ -0,0 +1,58 @@
+//#include<iostream>
+//using namespace std;
+//#include<list>
+//
+//
+//int main()
+//{
+//	int n = 0;
+//	while (cin >> n)
+//	{
+//		list<int> l;
+//
+//		for (int i = 0; i <= n; i++)
+//		{
+//			l.push_back(i);
+//		}
+//
+//		while (l.size() != 1)
+//		{
+//			int num = 1;
+//			list<int>::iterator it = l.begin();
+//			while (it != l.end())
+//			{
+//				if (num)
+//				{
+//					it = l.erase(it);
+//					num = 0;
+//					continue;
+//				}
+//
+//				num = 1;
+//				it++;
+//			}
+//		}
+//
+//		cout << l.front() << endl;
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<set>
+
+int main()
+{
+
+	int n, m;
+	cin >> n >> m;
+
+
+	return 0;
+}
